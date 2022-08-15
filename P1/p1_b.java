cca40616bc0ba711e015497d0c6b14b4e49e3e97
Abhijith Mallya <abhijithmallya@gmail.com>
2022-08-15 09:42:43 +0530

    P1_a

---
+++ P1/p1_b.java
@@ -0,0 +1,5 @@
+public class p1_b {
+    public static void main (String args[]){
+
+    }
+}
---
+++ P6/Prog6a.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class KnapsackDP 
+{
+    public void solve ( int[] wt , int[] val , int w , int n)  
+    {
+        int i,j ; 
+        int[][] sol = new int[n+1][w+1] ;
+        for(i=0;i<=n;i++);
+        {
+            
+        }
+    }
+}
