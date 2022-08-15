b8b0ad8383ae92a2f3af3c875798b35daea5b14c
Abhijith Mallya <abhijithmallya@gmail.com>
2022-08-15 15:11:55 +0530

    2_A --> Staff (Teach/Tech/Contr)

--- P2/p2_a.java
+++ P2/p2_a.java
@@ -5,8 +5,12 @@
  * 2. Technical(skills)
  * 3. Contract( period )
  * 
+ * StaffId  Name    Phone   salary     Domain    Publication     Skills     Period     
+ * 
  */
 import java.util.*;
+
+import javax.print.attribute.standard.Sides;
 
 class Staff {
     String StaffId, Name, Phone;
@@ -21,7 +25,7 @@ class Staff {
     }
 
     public void display() {
-        System.out.println(StaffId + "\t\t" + Name + "\t\t" + Phone + "\t\t" + Salary + "\t\t");
+        System.out.print(StaffId + "\t\t" + Name + "\t\t" + Phone + "\t\t" + Salary + "\t\t");
     }
 }
 
@@ -35,27 +39,55 @@ class Teaching extends Staff {
 
     }
 
-    
-
-}
-
-public class p2_a {
-    public static void main(String[] args) {
-        ;
+    public void display() {
+        super.display();
+        System.out.println(domain + "\t\t" + publication + "\t\t*****\t\t*****");
     }
 
 }
 
+class Technical extends Staff {
+    String skills;
 
+    Technical(String sid, String nm, String phn, int slry, String skl) {
+        super(sid, nm, phn, slry);
+        skills = skl;
 
+    }
 
+    public void display() {
+        super.display();
+        System.out.println("*****\t\t*****\t\t" + skills + "\t\t*****");
+    }
 
+}
 
+class Contract extends Staff {
+    int Period;
 
+    Contract(String sid, String nm, String phn, int slry, int prd) {
+        super(sid, nm, phn, slry);
+        Period = prd;
+    }
 
+    public void display() {
+        super.display();
+        System.out.println("*****\t\t*****\t\t*****\t\t" + Period);
+    }
+}
 
+public class p2_a {
+    public static void main(String[] args) {
+        Staff[] s = new Staff[3];
+        // StaffId Name Phone salary Domain Publication Skills Period
 
+        s[0] = new Teaching("101", "Abhi", "5432", 12000, "AIML", "OPEN CV");
+        s[1] = new Technical("102", "Glen", "6323", 22000, "ROS");
+        s[2] = new Contract("103", "XYZ", "2341", 2000, 12);
+        System.out.println("StaffId\t\tName\t\tPhone\t\tSalary\t\tDomain\t\tPublication\t\tSkills\t\tPeriod");
+        for (int i = 0; i < 3; i++)
+            s[i].display();
 
+    }
 
-
-
+}
