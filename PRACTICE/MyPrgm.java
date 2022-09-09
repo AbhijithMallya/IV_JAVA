ae3ba9042f5b8e2471a26988ca9f1a875bde12c9
= <=>
2022-09-09 16:12:55 +0530

    P1

---
+++ PRACTICE/MyPrgm.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Student
+{
+    String USN , Name , Branch , Phone ;
+    public void 
+}
+public class Myprgm
+{
+    public static void main(String[] args) {
+        
+     
+    }
+}
