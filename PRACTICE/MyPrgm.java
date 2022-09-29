9d7f6b845e5a960ae1c3ecf10cd473a78639b018
= <=>
2022-09-29 19:10:31 +0530

    Practice Swings

--- PRACTICE/MyPrgm.java
+++ PRACTICE/MyPrgm.java
@@ -1,14 +1,23 @@
+import java.awt.*;
 import java.util.*;
+import javax.swing.*;
 
-class Student
-{
-    String USN , Name , Branch , Phone ;
-    public void 
+
+
+class beta_gamma {
+  beta_gamma()
+  {
+    JFrame jf = new JFrame("BETA GAMMA");
+    JTextField jtf = new JTextField();
+    jf.setBounds(300,300,400,400);
+    jf.setVisible(true);
+  }
 }
-public class Myprgm
-{
-    public static void main(String[] args) {
-        
-     
-    }
+
+public class MyPrgm {
+
+  public static void main(String[] args) {
+
+    beta_gamma obj = new beta_gamma();
+  }
 }
